aa30cd4f24cf011aa8fd7be8ee4c2e0411cc0164
Phmik <phm@live.no>
2022-02-21 22:17:41 +0100

    Update if else

--- js/details.js
+++ js/details.js
@@ -9,17 +9,22 @@ const mountId = params.get("id")
 const idURL = "https://ffxivcollect.com/api/mounts/" + mountId;
 
 async function fetchMount() {
+
     try {
         
         const response = await fetch(idURL);
 
         const results = await response.json();
-
-        createHtml(results)
-
+        console.log(results)
+        if(results.error) {
+            mountDetails.innerHTML = displayError(`Ouch. Problem loading the API <p>${results.error}: Error ${results.status}</p>`)
+        }
+        else {
+        createHtml(results);
+        }
     }
     catch(error) {
-        mountDetails.innerHTML = displayError("Error loading the content from the API")
+        mountDetails.innerHTML = displayError("Error loading the content from the API");
     }
 }
 
@@ -27,6 +32,7 @@ fetchMount();
 
 function createHtml(results) {
     try {
+        
     document.title = "";
     document.title = `${results.name}`
     mountDetails.innerHTML = `
@@ -41,10 +47,10 @@ function createHtml(results) {
                                     <li>Owned: ${results.owned}</li>
                                     <li>ID: ${results.id}</li>
                                 </ul>
+                                <p>${results.tooltip}
                             </div>`; 
-        }
-        catch(error) {
-            mountDetails.innerHTML = displayError("Error loading the content from the API")
-        }
-    
+    }
+    catch(error){
+    mountDetails.innerHTML = displayError("Error loading the content from the API");
+    }
 }
