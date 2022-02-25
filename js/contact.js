affb14f7bace13527a0d8046fd0609d3115548ee
Phmik <phm@live.no>
2022-02-25 13:12:56 +0100

    delivery ready

--- js/contact.js
+++ js/contact.js
@@ -8,47 +8,47 @@ const addressError = document.querySelector("#addressError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
 const contactButton = document.querySelector(".contact-button");
-const validateSuccess = document.querySelector(".contact-h2");
+const validateSuccess = document.querySelector(".success");
 
 
 contactForm.addEventListener("submit", validateForm);
 
 function validateForm(event) {
+
     event.preventDefault();
-   try {
-   if(validateName(fullName.value.trim()) === true) {
-        fullNameError.style.display = "none";
-    } else {
-        fullNameError.style.display = "block";
-    }
-
-    if(checkLength(subject.value, 10) === true){
-        subjectError.style.display = "none";
-    } else {
-        subjectError.style.display = "block";
-    }
-
-    if(checkLength(address.value, 25) === true){
-        addressError.style.display = "none";
-    } else {
-        addressError.style.display = "block";
-    }
-
-    if(validateEmail(email.value) === true){
-        emailError.style.display = "none";
-    } else {
-        emailError.style.display = "block";
-    }
-
-
+    try {
+        if(validateName(fullName.value.trim()) === true) {
+                fullNameError.style.display = "none";
+                validationSuccess++;
+            } else {
+                fullNameError.style.display = "block";
+            }
+            if(checkLength(subject.value, 10) === true){
+                subjectError.style.display = "none";
+                validationSuccess++;
+            } else {
+                subjectError.style.display = "block";
+            }
+            if(checkLength(address.value, 25) === true){
+                addressError.style.display = "none";
+                validationSuccess++;
+            } else {
+                addressError.style.display = "block";
+            }
+            if(validateEmail(email.value) === true){
+                emailError.style.display = "none";
+                validationSuccess++;
+            } else {
+                emailError.style.display = "block";
+            }
     } catch(error) {
         displayError("Please check your details")
     }
+    if(validateName(fullName.value.trim()) && checkLength(subject.value, 10) && checkLength(address.value, 25) && validateEmail(email.value)){
+        contactForm.reset();
+        validateSuccess.innerHTML = `<h2 class="success-h2">Validation Successful</h2>`;
+    }
 }
-    
-
-
-
 
 function checkLength(value, characters) {
     if (value.trim().length > characters) {
@@ -65,12 +65,8 @@ function validateEmail(email) {
     }
 
 
-    function validateName(name) {
+function validateName(name) {
        const letters = /^[A-Za-z ]+$/;
        const letterMatches = letters.test(name)
        return letterMatches;
-    }
-
-    function validateSuccessful() {
-        validateSuccess.style.display = "block";
     }
